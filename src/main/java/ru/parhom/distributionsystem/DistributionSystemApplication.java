48b9c14048f2f8ef943dc9215f97f8b8a707c2fe
Yrii Parhom <parhomenko.yurij@gmail.com>
2023-05-30 16:07:51 +0300

    iinitial commit

---
+++ src/main/java/ru/parhom/distributionsystem/DistributionSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.parhom.distributionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/parhom/distributionsystem/DistributionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parhom.distributionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
