31d167e074534a65872703dc9a6fca5015d0c0cb
Yrii Parhom <parhomenko.yurij@gmail.com>
2023-07-02 21:11:08 +0300

    interim commit

--- src/main/java/ru/parhom/distributionsystem/configuration/JwtAuthenticationFilter.java
+++ src/main/java/ru/parhom/distributionsystem/configuration/JwtAuthenticationFilter.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +20,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -36,5 +40,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt = authHeader.substring(7);
 
         userLogin = jwtService.extractUserLogin(jwt);
+        if(userLogin != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+          //  UserDetails userDetails = this.userDetailsService.loadByUsername(userLogin);
+        }
     }
 }
